ae32a740fccec6c2559f67a1a9541f3c31ac7cae
Danya-web <danilameretov20@gmail.com>
2025-03-13 21:11:50 +0500

    some changes

--- src/components/custom/Task.tsx
+++ src/components/custom/Task.tsx
@@ -1,6 +1,7 @@
 import { GripVertical } from "lucide-react"
 import { Card, CardContent } from "../ui/card"
 import { cn } from "@/lib/utils"
+import { useState } from "react"
 
 interface TaskProps {
   id: string;
@@ -8,15 +9,20 @@ interface TaskProps {
 }
 
 export const Task: React.FC<TaskProps> = ({ id, title }) => {
-  const handleDragStart = (e: React.DragEvent) => {
-    e.dataTransfer.setData("taskId", id);
-  }
+  const [className, setClassName] = useState("")
 
   return (
     <Card
       draggable
-      onDragStart={handleDragStart}
-      className={cn("bg-white shadow-sm flex items-center p-3 gap-3")}
+      onDragStart={(e) => {
+        e.dataTransfer.setData("taskId", id);
+        setClassName("bg-red-500");
+        setTimeout(() => {
+          setClassName("hidden");
+        }, 0);
+      }}
+      onDragEnd={() => setClassName("flex")}
+      className={cn("bg-white shadow-sm flex items-center p-3 gap-3", className)}
     >
       <GripVertical className="text-gray-400" size={16} />
       <CardContent className="p-0 flex-1">{title}</CardContent>
